63b5ab74195e00ad7fe2cb1369ddd2eecf375cec
David Darj <zid@alagazam.net>
2022-12-04 19:06:43 +0100

    Day 04 complete

---
+++ Day04/Day04.cs
@@ -0,0 +1,51 @@
+﻿using System.Diagnostics;
+
+namespace AoC2020
+{
+    public static class Day04
+    {
+        public static Int64 Day04a(string[] input)
+        {
+            var num = 0;
+            foreach (var item in input)
+            {
+                var groups = item.Split(',');
+                var elf1 = groups[0].Split('-');
+                var elf2 = groups[1].Split('-');
+                var x1 = Convert.ToUInt64(elf1[0]);
+                var x2 = Convert.ToUInt64(elf1[1]);
+                var y1 = Convert.ToUInt64(elf2[0]);
+                var y2 = Convert.ToUInt64(elf2[1]);
+                if (x1 <= y1 && x2 >= y2 || y1 <= x1 && y2 >= x2) num++;
+            }
+            return num;
+        }
+
+        public static Int64 Day04b(string[] input)
+        {
+            var num = 0;
+            foreach (var item in input)
+            {
+                var groups = item.Split(',');
+                var elf1 = groups[0].Split('-');
+                var elf2 = groups[1].Split('-');
+                var x1 = Convert.ToUInt64(elf1[0]);
+                var x2 = Convert.ToUInt64(elf1[1]);
+                var y1 = Convert.ToUInt64(elf2[0]);
+                var y2 = Convert.ToUInt64(elf2[1]);
+                if (x2 >= y1 && x1 <= y2) num++;
+            }
+            return num;
+        }
+
+
+        static void Main(string[] args)
+        {
+            var lines = File.ReadAllLines("Day04.txt");
+            var sw = Stopwatch.StartNew();
+            Console.WriteLine("Day04a : {0}   Time: {1}", Day04a(lines), sw.ElapsedMilliseconds);
+            sw.Restart();
+            Console.WriteLine("Day04b : {0}   Time: {1}", Day04b(lines), sw.ElapsedMilliseconds);
+        }
+    }
+}
---
+++ Day04/Day04Test.cs
@@ -0,0 +1,54 @@
+using System.Diagnostics;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace AoC2020
+{
+    public class Day04Test
+    {
+        readonly string input =
+@"2-4,6-8
+2-3,4-5
+5-7,7-9
+2-8,3-7
+6-6,4-6
+2-6,4-8
+";
+
+        readonly Int64 resultA = 2;
+        readonly Int64 resultB = 4;
+
+        [Fact]
+        public void Day04a()
+        {
+            var sw = Stopwatch.StartNew();
+            var lines = input.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries);
+
+            var result = Day04.Day04a(lines);
+            Assert.Equal(resultA, result);
+
+            Console.WriteLine("Day04a : {0}   Time: {1}", result, sw.ElapsedMilliseconds);
+        }
+
+
+        [Fact]
+        public void Day04b()
+        {
+            var sw = Stopwatch.StartNew();
+            var lines = input.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries);
+
+            var result = Day04.Day04b(lines);
+            Assert.Equal(resultB, result);
+
+            Console.WriteLine("Day04b : {0}   Time: {1}", result, sw.ElapsedMilliseconds);
+        }
+
+        public Day04Test(ITestOutputHelper output)
+        {
+            var converter = new AoCUtils.Converter(output);
+            Console.SetOut(converter);
+        }
+
+    }
+
+}
