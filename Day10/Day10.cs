9fb4ef500bcea54c3b8749a87de1df17e709c348
David Darj <zid@alagazam.net>
2022-12-10 17:56:05 +0100

    Day 10 complete

---
+++ Day10/Day10.cs
@@ -0,0 +1,101 @@
+﻿using System.Diagnostics;
+using System.Text;
+
+namespace AoC2020
+{
+    public static class Day10
+    {
+        public static Int64 Day10a(string[] input)
+        {
+            var checkpoints = new List<Int64>() { 20, 60, 100, 140, 180, 220 };
+            Int64 result = 0;
+            Int64 clock = 1;
+            int cycles = 0;
+            Int64 addvalue = 0;
+            Int64 x = 1;
+            foreach (var row in input)
+            {
+                var cmd = row.Split(' ');
+                switch (cmd[0])
+                {
+                    case "noop":
+                        cycles = 1;
+                        addvalue = 0;
+                        break;
+                    case "addx":
+                        cycles = 2;
+                        addvalue = Convert.ToInt64(cmd[1]);
+                        break;
+                }
+                while (cycles != 0)
+                {
+                    clock++;
+                    cycles--;
+                    if (cycles == 0) { x += addvalue; }
+                    if (checkpoints.Contains(clock))
+                    {
+                        Console.WriteLine("{0} {1} {2}", clock, x, clock * x);
+                        result += clock*x; 
+                    }
+                }
+            }
+            return result;
+        }
+
+        public static String Day10b(string[] input)
+        {
+            var checkpoints = new List<Int64>() { 20, 60, 100, 140, 180, 220 };
+            StringBuilder result = new StringBuilder(300);
+            Int64 clock = 1;
+            Int64 pixel = 0;
+            int cycles = 0;
+            Int64 addvalue = 0;
+            Int64 x = 1;
+            foreach (var row in input)
+            {
+                var cmd = row.Split(' ');
+                switch (cmd[0])
+                {
+                    case "noop":
+                        cycles = 1;
+                        addvalue = 0;
+                        break;
+                    case "addx":
+                        cycles = 2;
+                        addvalue = Convert.ToInt64(cmd[1]);
+                        break;
+                }
+                while (cycles != 0)
+                {
+                    clock++;
+                    cycles--;
+                    if (pixel >= x-1 && pixel <= x+1)
+                    {
+                        result.Append('#');
+                    }
+                    else
+                    {
+                        result.Append('.');
+                    }
+                    if (pixel == 39)
+                    {
+                        result.Append("\r\n");
+                    }
+                    pixel = (pixel + 1) % 40;
+                    if (cycles == 0) { x += addvalue; }
+                }
+            }
+            return result.ToString();
+        }
+
+
+        static void Main(string[] args)
+        {
+            var lines = File.ReadAllLines("Day10.txt");
+            var sw = Stopwatch.StartNew();
+            Console.WriteLine("Day10a : {0}   Time: {1}", Day10a(lines), sw.ElapsedMilliseconds);
+            sw.Restart();
+            Console.WriteLine("Day10b : \r\n{0}   Time: {1}", Day10b(lines), sw.ElapsedMilliseconds);
+        }
+    }
+}
---
+++ Day10/Day10Test.cs
@@ -0,0 +1,201 @@
+using System.Diagnostics;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace AoC2020
+{
+    public class Day10Test
+    {
+        readonly string input =
+@"addx 15
+addx -11
+addx 6
+addx -3
+addx 5
+addx -1
+addx -8
+addx 13
+addx 4
+noop
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx -35
+addx 1
+addx 24
+addx -19
+addx 1
+addx 16
+addx -11
+noop
+noop
+addx 21
+addx -15
+noop
+noop
+addx -3
+addx 9
+addx 1
+addx -3
+addx 8
+addx 1
+addx 5
+noop
+noop
+noop
+noop
+noop
+addx -36
+noop
+addx 1
+addx 7
+noop
+noop
+noop
+addx 2
+addx 6
+noop
+noop
+noop
+noop
+noop
+addx 1
+noop
+noop
+addx 7
+addx 1
+noop
+addx -13
+addx 13
+addx 7
+noop
+addx 1
+addx -33
+noop
+noop
+noop
+addx 2
+noop
+noop
+noop
+addx 8
+noop
+addx -1
+addx 2
+addx 1
+noop
+addx 17
+addx -9
+addx 1
+addx 1
+addx -3
+addx 11
+noop
+noop
+addx 1
+noop
+addx 1
+noop
+noop
+addx -13
+addx -19
+addx 1
+addx 3
+addx 26
+addx -30
+addx 12
+addx -1
+addx 3
+addx 1
+noop
+noop
+noop
+addx -9
+addx 18
+addx 1
+addx 2
+noop
+noop
+addx 9
+noop
+noop
+noop
+addx -1
+addx 2
+addx -37
+addx 1
+addx 3
+noop
+addx 15
+addx -21
+addx 22
+addx -6
+addx 1
+noop
+addx 2
+addx 1
+noop
+addx -10
+noop
+noop
+addx 20
+addx 1
+addx 2
+addx 2
+addx -6
+addx -11
+noop
+noop
+noop
+";
+
+        readonly Int64 resultA = 13140;
+        readonly String resultB =
+@"##..##..##..##..##..##..##..##..##..##..
+###...###...###...###...###...###...###.
+####....####....####....####....####....
+#####.....#####.....#####.....#####.....
+######......######......######......####
+#######.......#######.......#######.....
+";
+
+        [Fact]
+        public void Day10a()
+        {
+            var sw = Stopwatch.StartNew();
+            var lines = input.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries);
+
+            var result = Day10.Day10a(lines);
+            Assert.Equal(resultA, result);
+
+            Console.WriteLine("Day10a : {0}   Time: {1}", result, sw.ElapsedMilliseconds);
+        }
+
+
+        [Fact]
+        public void Day10b()
+        {
+            var sw = Stopwatch.StartNew();
+            var lines = input.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries);
+
+            var result = Day10.Day10b(lines);
+            Assert.Equal(resultB, result);
+
+            Console.WriteLine("Day10b : \r\n{0}   Time: {1}", result, sw.ElapsedMilliseconds);
+        }
+
+        public Day10Test(ITestOutputHelper output)
+        {
+            var converter = new AoCUtils.Converter(output);
+            Console.SetOut(converter);
+        }
+
+    }
+
+}
